5358820fc40f689da2a3d3b5a56115da7450ab8c
Abdulbaset R. Sayed <AbdulBasetRedaSayedHF@gmail.com>
2025-09-22 19:13:51 +0300

    update

--- resources/views/admin/dashboard.blade.php
+++ resources/views/admin/dashboard.blade.php
@@ -1,40 +1,21 @@
-<!DOCTYPE html>
-<html lang="{{ app()->getLocale() }}">
+@extends('admin.structure')
 
-<head>
-    <meta charset="utf-8">
-    <meta http-equiv="X-UA-Compatible" content="IE=edge">
-    <meta name="viewport" content="width=device-width, initial-scale=1">
-    <meta name="csrf-token" content="{{ csrf_token() }}">
-    <title>{{ config('app.name', 'Laravel') }}</title>
-    <link rel="icon" type="image/png" sizes="16x16" href="{{ asset('general/images/favicon.png') }}">
+@section('title', 'Dashboard')
 
-    <!-- Fonts -->
-    <link rel="stylesheet" href="{{ asset('general/fonts/fonts.css') }}">
-    <link rel="stylesheet" href="{{ asset('general/fontawesome/fontawesome-free-6.4.2-web/css/all.css') }}">
-    <link rel="stylesheet"
-        href="{{ asset('general/libraries/bootstrap/bootstrap-v5.3.8/dist/css/bootstrap.min.css') }}">
-    <link rel="stylesheet" href="{{ asset('general/libraries/sweetAlert2/v11.17.2/sweetalert2.min.css') }}">
+@section('navbar')
+    <x-admin.navbar />
+@endsection
 
-    <!-- Site Stylesheet -->
-    <link href="{{ asset('css/app.css') }}" rel="stylesheet">
-</head>
+@section('footer')
+    <x-admin.footer />
+@endsection
 
-<body class="bg-gray-100 font-default">
+@section('content')
     <div class="container">
-        <h1>Admin Dashboard</h1>
+        <h1>Admin Dashboard.</h1>
         <form action="{{ route('admin.logout') }}" method="POST">
             @csrf
             <button type="submit">Logout</button>
         </form>
     </div>
-
-    <!-- Site Scripts -->
-    <script src="{{ asset('js/app.js') }}"></script>
-    <script src="{{ asset('general/libraries/bootstrap/bootstrap-v5.3.8/dist/js/bootstrap.bundle.min.js') }}"></script>
-    <script src="{{ asset('general/libraries/jquery/jquery-3.7.1/minified.jquery-3.7.1.js') }}"></script>
-    <script src="{{ asset('general/libraries/chart.js/chart-v4.5.0.js') }}"></script>
-    <script src="{{ asset('general/libraries/sweetAlert2/v11.17.2/sweetalert2.min.js') }}"></script>
-</body>
-
-</html>
+@endsection
---
+++ resources/views/admin/structure.blade.php
@@ -0,0 +1,42 @@
+<!DOCTYPE html>
+<html lang="{{ app()->getLocale() }}" class="h-100">
+
+<head>
+    <!-- Meta Data -->
+    <meta charset="utf-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <meta name="csrf-token" content="{{ csrf_token() }}">
+    <title>{{ env('APP_NAME') }} @if(trim($__env->yieldContent('title'))) | @yield('title') @endif</title>
+    @yield('meta')
+    <link rel="icon" type="image/png" sizes="16x16" href="{{ asset('general/images/favicon.png') }}">
+
+    <!-- Fonts -->
+    <link rel="stylesheet" href="{{ asset('general/fonts/fonts.css') }}">
+    <link rel="stylesheet" href="{{ asset('general/fontawesome/fontawesome-free-6.4.2-web/css/all.css') }}">
+    <!-- Site Stylesheet -->
+    <link rel="stylesheet" href="{{ asset('general/libraries/bootstrap/bootstrap-v5.3.8/dist/css/bootstrap.min.css') }}">
+    <link rel="stylesheet" href="{{ asset('general/libraries/sweetAlert2/v11.17.2/sweetalert2.min.css') }}">
+    <link href="{{ asset('css/app.css') }}" rel="stylesheet">
+    @yield('main.style')
+
+</head>
+
+<body class="font-default" dir="ltr">
+
+    @yield('navbar')
+
+    @yield('content')
+
+    @yield('footer')
+
+    <!-- Site Scripts -->
+    @yield('main.script')
+    <script src="{{ asset('js/app.js') }}"></script>
+    <script src="{{ asset('general/libraries/bootstrap/bootstrap-v5.3.8/dist/js/bootstrap.bundle.min.js') }}"></script>
+    <script src="{{ asset('general/libraries/jquery/jquery-3.7.1/minified.jquery-3.7.1.js') }}"></script>
+    <script src="{{ asset('general/libraries/chart.js/chart-v4.5.0.js') }}"></script>
+    <script src="{{ asset('general/libraries/sweetAlert2/v11.17.2/sweetalert2.min.js') }}"></script>
+</body>
+
+</html>
