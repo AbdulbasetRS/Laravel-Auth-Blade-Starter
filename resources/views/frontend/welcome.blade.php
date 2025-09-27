4f4b15ea4a049fb2ca85dc16667f6f4fd676f92b
Abdulbaset R. Sayed <AbdulBasetRedaSayedHF@gmail.com>
2025-09-27 18:07:57 +0300

    update

--- resources/views/frontend/welcome.blade.php
+++ resources/views/frontend/welcome.blade.php
@@ -8,13 +8,13 @@
     <meta name="csrf-token" content="{{ csrf_token() }}">
     <title>{{ env('APP_NAME') }} @if(trim($__env->yieldContent('title'))) | @yield('title') @endif</title>
     @yield('meta')
-    <link rel="icon" type="image/png" sizes="16x16" href="{{ asset('general/images/favicon.png') }}">
+    <link rel="icon" type="image/png" sizes="16x16" href="{{ asset('assets/images/favicon.png') }}">
 
     <!-- Fonts -->
-    <link rel="stylesheet" href="{{ asset('general/fonts/fonts.css') }}">
-    <link rel="stylesheet" href="{{ asset('general/fontawesome/fontawesome-free-6.4.2-web/css/all.css') }}">
-    <link rel="stylesheet" href="{{ asset('general/libraries/bootstrap/bootstrap-v5.3.8/dist/css/bootstrap.min.css') }}">
-    <link rel="stylesheet" href="{{ asset('general/libraries/sweetAlert2/v11.17.2/sweetalert2.min.css') }}">
+    <link rel="stylesheet" href="{{ asset('assets/fonts/fonts.css') }}">
+    <link rel="stylesheet" href="{{ asset('assets/fontawesome/fontawesome-free-6.4.2-web/css/all.css') }}">
+    <link rel="stylesheet" href="{{ asset('assets/libraries/bootstrap/bootstrap-v5.3.8/dist/css/bootstrap.min.css') }}">
+    <link rel="stylesheet" href="{{ asset('assets/libraries/sweetAlert2/v11.17.2/sweetalert2.min.css') }}">
     
     <!-- Site Stylesheet -->
     <link href="{{ asset('css/app.css') }}" rel="stylesheet">
@@ -36,10 +36,10 @@
 
     <!-- Site Scripts -->
     <script src="{{ asset('js/app.js') }}"></script>
-    <script src="{{ asset('general/libraries/bootstrap/bootstrap-v5.3.8/dist/js/bootstrap.bundle.min.js') }}"></script>
-    <script src="{{ asset('general/libraries/jquery/jquery-3.7.1/minified.jquery-3.7.1.js') }}"></script>
-    <script src="{{ asset('general/libraries/chart.js/chart-v4.5.0.js') }}"></script>
-    <script src="{{ asset('general/libraries/sweetAlert2/v11.17.2/sweetalert2.min.js') }}"></script>
+    <script src="{{ asset('assets/libraries/bootstrap/bootstrap-v5.3.8/dist/js/bootstrap.bundle.min.js') }}"></script>
+    <script src="{{ asset('assets/libraries/jquery/jquery-3.7.1/minified.jquery-3.7.1.js') }}"></script>
+    <script src="{{ asset('assets/libraries/chart.js/chart-v4.5.0.js') }}"></script>
+    <script src="{{ asset('assets/libraries/sweetAlert2/v11.17.2/sweetalert2.min.js') }}"></script>
     @yield('main.script')
 </body>
 
